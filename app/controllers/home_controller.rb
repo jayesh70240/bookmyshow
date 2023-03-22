7e5d546cb5f86363bb30a4933bccea0cbe65b037
Jayesh Rathore <jayeshharda.com>
2023-03-22 20:35:38 +0530

    creating home page controllers

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,37 @@
+class HomeController < ApplicationController
+	def index
+  end
+
+  def movies
+  end
+
+  def stream
+  end
+
+  def events
+  end
+
+  def plays
+  end
+
+  def sports
+  end
+
+  def activities
+  end
+
+  def buzz
+  end
+
+  def listyourshow
+  end
+
+  def corporates
+  end
+
+  def giftcards
+  end
+
+  def offers
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'home#index'
+  get "home/movies", to: "home#movies"
+  get "home/stream", to: "home#stream"
+  get "home/events", to: "home#events"
+  get "home/plays", to: "home#plays"
+  get "home/sports", to: "home#sports"
+  get "home/activities", to: "home#activities"
+  get "home/buzz", to: "home#buzz"
+  get "home/listyourshow", to: "home#listyourshow"
+  get "home/corporates", to: "home#corporates"
+  get "home/offers", to: "home#offers"
+  get "home/giftcards", to: "home#giftcards"
 end
